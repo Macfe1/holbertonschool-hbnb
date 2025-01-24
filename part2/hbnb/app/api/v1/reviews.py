4e219a793e635de05dfc1179b589672a73d9b89c
carlos13suarez <charliesuarez10@gmail.com>
2025-01-23 20:33:15 -0500

    update reviews

--- part2/hbnb/app/api/v1/reviews.py
+++ part2/hbnb/app/api/v1/reviews.py
@@ -1,5 +1,7 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+import json
+from flask import request
 
 api = Namespace('reviews', description='Review operations')
 
