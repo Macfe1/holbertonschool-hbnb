1d4e7dec927fdd5bffbacdd7973640db11a32fdc
carlos13suarez <charliesuarez10@gmail.com>
2025-02-09 22:50:25 -0500

    update SQLAlchemyRepository model so that there are no circular imports

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from app import db
 
 class Repository(ABC):
     @abstractmethod
@@ -25,64 +24,45 @@ class Repository(ABC):
     @abstractmethod
     def get_by_attribute(self, attr_name, attr_value):
         pass
-"""
-#In Memory Repository
 
-class InMemoryRepository(Repository):
-    def __init__(self):
-        self._storage = {}
 
-    def add(self, obj):
-        self._storage[obj.id] = obj
-
-    def get(self, obj_id):
-        return self._storage.get(obj_id)
-
-    def get_all(self):
-        return list(self._storage.values())
-
-    def update(self, obj_id, data):
-        obj = self.get(obj_id)
-        if obj:
-            obj.update(data)
-            self._storage[obj_id] = obj
-        return obj
-
-    def delete(self, obj_id):
-        if obj_id in self._storage:
-            del self._storage[obj_id]
-
-    def get_by_attribute(self, attr_name, attr_value):
-        return next((obj for obj in self._storage.values() if getattr(obj, attr_name) == attr_value), None)
-"""
-# SQLAlchemy Repository
-
-class SQLAlchemyRepository(Repository):
+class SQLAlchemyRepository:
     def __init__(self, model):
+        """Initialize with a SQLAlchemy model."""
         self.model = model
 
     def add(self, obj):
+        """Adds an object to the database."""
+        from app import db  # ✅ Lazy import `db` to avoid circular import
         db.session.add(obj)
         db.session.commit()
 
     def get(self, obj_id):
+        """Retrieves an object by its ID."""
         return self.model.query.get(obj_id)
 
     def get_all(self):
+        """Retrieves all objects of this type."""
         return self.model.query.all()
 
     def update(self, obj_id, data):
+        """Updates an existing object with new data."""
+        from app import db  # ✅ Lazy import `db`
         obj = self.get(obj_id)
         if obj:
             for key, value in data.items():
                 setattr(obj, key, value)
             db.session.commit()
+            return obj
 
     def delete(self, obj_id):
+        """Deletes an object from the database."""
+        from app import db  # ✅ Lazy import `db`
         obj = self.get(obj_id)
         if obj:
             db.session.delete(obj)
             db.session.commit()
 
     def get_by_attribute(self, attr_name, attr_value):
+        """Retrieves an object based on a specific attribute."""
         return self.model.query.filter_by(**{attr_name: attr_value}).first()
