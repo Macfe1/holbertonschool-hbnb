3f162c4bf48f46114db8f3d86ed2089e7292f0be
Macfe1 <mac1515.pp@gmail.com>
2025-03-11 12:53:11 -0500

    Eliminando archivos innecesarios

--- part2/hbnb/tests/test_amenity.py
+++ part2/hbnb/tests/test_amenity.py
@@ -1,10 +1,13 @@
 from app.models.amenity import Amenity
 from app.models.basemodel import BaseModel
+import unittest
 
+class TestAmenity(unittest.TestCase):
 
-def test_amenity_creation():
-    amenity = Amenity(name="Wi-Fi")
-    assert amenity.name == "Wi-Fi"
-    print("Amenity creation test passed!")
+    def test_amenity_creation(self):
+        amenity = Amenity(name="Wi-Fi")
+        assert amenity.name == "Wi-Fi"
+        print("Amenity creation test passed!")
 
-test_amenity_creation()
+if __name__ == '__main__':
+    unittest.main()
